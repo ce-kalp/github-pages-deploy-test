c9884b729f0e46c9998d8cd491ae1a0999321d40
ce-kalp <kkhadela@techechelons.com>
2024-09-03 18:59:12 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+
+module.exports = {
+  // ... your existing configuration ...
+  resolve: {
+    fallback: {
+      "crypto": false
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+  node: {
+    crypto: true,
+  }
+};
