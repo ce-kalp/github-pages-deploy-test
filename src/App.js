9bd22ff1a7b9194525676cc57f54bedcf29989ea
ce-kalp <kkhadela@techechelons.com>
2024-09-04 11:06:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,6 @@
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-// import "./App.css";
 import "./AppV1.css";
 
-// Version 1 Using Library
 function App() {
-  const images = [
-    "./images/image1.png",
-    "./images/image2.png",
-    "./images/image3.png",
-    "./images/image3.png",
-    "./images/image1.png",
-
-    "./images/image2.png",
-    "./images/image3.png",
-    "./images/image4.png",
-    "./images/image2.png",
-
-    "./images/image1.png",
-
-    "./images/image1.png",
-    "./images/image5.png",
-    "./images/image2.png",
-    "./images/image3.png",
-  ];
-  return (
-    <div className="App">
-      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 3, 900: 5 }}>
-        <Masonry gutter="10px">
-          {images.map((image, i) => (
-            <img
-              key={i}
-              src={image}
-              alt={"image-" + i}
-              style={{ width: "100%", display: "block" }}
-            />
-          ))}
-        </Masonry>
-      </ResponsiveMasonry>
-    </div>
-  );
-}
-
-// Version 2 Using Custom Library built on top of Masonry
-function AppV1() {
   const images = [
     "./images/image1.png",
     "./images/image2.png",
@@ -75,5 +33,4 @@ function AppV1() {
   );
 }
 
-// export default App;
-export default AppV1;
+export default App;
